6e70d153d09473d06dbc7c90a7fc05a7d24ec9ee
h-latta <h.latta@yahoo.com>
2022-04-04 09:31:53 -0500

    Created Component Files, Installed Axios

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
+import NavBar from './Components/NavBar/NavBar';
 
 function App() {
+
+  const [Games, SetGames] = useState([])
+
+  async function getAllGames(){
+    const search = await axios.get(`https://localhost:7260`)
+    SetGames(search.data.items)
+    console.log(Games)
+  }
+
   return (
     <div className="App">
+      <nav><NavBar /></nav>
       <h1>Hello world!</h1>
     </div>
   );
---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,9 @@
+const NavBar = (props) => {
+    return ( 
+        <div>
+            <h2>Hello world!</h2>
+        </div>
+     );
+}
+ 
+export default NavBar;
